27924c1cf4ade25c0eb0d333c4a883d8cffc5a98
Kevin Harvey <kevin.harvey@smilecareclub.com>
2015-03-29 22:03:02 -0500

    tweak the typo again

--- jmad/solos/tests/test_api.py
+++ jmad/solos/tests/test_api.py
@@ -11,7 +11,7 @@ class SoloAPITestCase(APITestCase):
         setup_models(self)
 
     def test_create_solo(self):
-        """ Test that we can post a solo
+        """ Test that we can create a solo
         """
         post_data= {
             'track': '/api/tracks/2/',

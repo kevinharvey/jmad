48533c2da64677bcf3c4a826fd9619cf4bc6239f
Kevin Harvey <kevin.harvey@smilecareclub.com>
2015-04-26 23:57:16 -0500

    Adds form elements, fake search results to index.html.

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -23,16 +23,32 @@ class StudentTestCase(LiveServerTestCase):
         # He knows he's in the right place because he can see the name of the site in the heading.
         brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
         self.assertEqual('JMAD', brand_element.text)
-        self.fail('Incomplete Test')
-
 
         # He sees the inputs of the search form, including labels and placeholders.
+        instrument_input = self.browser.find_element_by_css_selector('input#jmad-instrument')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector('label[for="jmad-instrument"]'))
+        self.assertEqual(instrument_input.get_attribute('placeholder'), 'i.e. trumpet')
+
+        artist_input = self.browser.find_element_by_css_selector('input#jmad-artist')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector('label[for="jmad-artist"]'))
+        self.assertEqual(artist_input.get_attribute('placeholder'), 'i.e. Davis')
 
         # He types in the name of his instrument and submits it.
+        instrument_input.send_keys('saxophone')
+        self.browser.find_element_by_css_selector('form button').click()
 
         # He sees too many search results...
+        search_results = self.browser.find_elements_by_css_selector('.jmad-search-result')
+        self.assertGreater(len(search_results), 2)
 
         # ...so he adds an artist to his search query and gets a more manageable list.
+        second_artist_input = self.browser.find_element_by_css_selector('input#jmad-artist')
+        second_artist_input.send_keys('Cannonball Adderley')
+        self.browser.find_element_by_css_selector('form button').click()
+
+        second_search_results = self.browser.find_elements_by_css_selector('.jmad-search-result')
+        self.assertEqual(len(second_search_results), 2)
+        self.fail('Incomplete Test')
 
         # He clicks on a search result.
 

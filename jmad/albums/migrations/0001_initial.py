30af93c2996f8680ca15beb40c5c560725a4e9ab
Kevin Harvey <kevin.harvey@smilecareclub.com>
2015-07-30 21:16:09 -0500

    Rebuilds migrations for launch

--- jmad/albums/migrations/0001_initial.py
+++ jmad/albums/migrations/0001_initial.py
@@ -13,20 +13,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Album',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('artist', models.CharField(max_length=100)),
-                ('slug', models.SlugField()),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('artist', models.CharField(max_length=200)),
+                ('slug', models.SlugField(max_length=200)),
             ],
+            options={
+                'ordering': ['name'],
+            },
         ),
         migrations.CreateModel(
             name='Track',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
                 ('track_number', models.PositiveIntegerField(null=True, blank=True)),
-                ('slug', models.SlugField()),
+                ('slug', models.SlugField(max_length=200)),
                 ('album', models.ForeignKey(to='albums.Album')),
             ],
+            options={
+                'ordering': ['album', 'track_number'],
+            },
         ),
     ]
--- jmad/solos/migrations/0001_initial.py
+++ jmad/solos/migrations/0001_initial.py
@@ -7,13 +7,23 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('albums', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Solo',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('artist', models.CharField(max_length=200)),
+                ('instrument', models.CharField(max_length=200)),
+                ('start_time', models.CharField(max_length=20, null=True, blank=True)),
+                ('end_time', models.CharField(max_length=20, null=True, blank=True)),
+                ('slug', models.SlugField()),
+                ('track', models.ForeignKey(to='albums.Track')),
             ],
+            options={
+                'ordering': ['track', 'start_time'],
+            },
         ),
     ]

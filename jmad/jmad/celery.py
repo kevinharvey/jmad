820988a1b85aaea0d81d195a4672038cc7f2e402
Kevin Harvey <kevin.harvey@smilecareclub.com>
2015-08-04 00:28:58 -0500

    Updates EVN variable for WSGI file

--- jmad/jmad/celery.py
+++ jmad/jmad/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jmad.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jmad.settings.local')
 
 from django.conf import settings
 
